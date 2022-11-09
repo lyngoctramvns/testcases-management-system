c71fc70d0f747516fa65c65b775c35dbdcb6eb3b
lyngoctramvns <lyngoctramvns@gmail.com>
2022-11-09 13:32:24 +0700

    case-study(cont)

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-function submitTestcases (){
+const fs = require('fs');
+
+function submitTestCases (){
+    let objective = document.getElementById('objective').value;
+    let stepOne = document.getElementById('step-1').value;
+    let stepTwo = document.getElementById('step-2').value;
+    let bug = document.getElementById('bug').value;
+
+    if(){
+
+    }else {
+        
+    }
+
+
+    const testcaseObject = {
+        "Objective": objective,
+        "Step 1": stepOne,
+        "Step 2": stepTwo,
+        "Bug Details": bug
+    };
+
+
+
 
 };

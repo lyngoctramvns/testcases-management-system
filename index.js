21b34acf0ac0262b9be0e96ce863ce0f04195f57
Ly Ngoc Tram <tramln@bsscommerce.com>
2022-11-10 01:06:21 +0700

    new

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-const fs = require('fs');
-
-function submitTestCases (){
+function submitTestCases() {
     let objective = document.getElementById('objective').value;
     let stepOne = document.getElementById('step-1').value;
     let stepTwo = document.getElementById('step-2').value;
     let bug = document.getElementById('bug').value;
 
-    if(){
-
-    }else {
-        
+    if(localStorage.getItem('id')){
+        localStorage.setItem('id', +( localStorage.getItem('id')) + 1)
+    } else {
+        localStorage.setItem('id', "0");
     }
 
-
-    const testcaseObject = {
-        "Objective": objective,
-        "Step 1": stepOne,
-        "Step 2": stepTwo,
-        "Bug Details": bug
+    let object = {
+        id: localStorage.getItem('id'),
+        Objective: objective,
+        Step1: stepOne,
+        Step2: stepTwo,
+        BugDetails: bug
     };
 
+    let array = JSON.parse(localStorage.getItem('TestCasesData'));
 
+    if(array){
+        localStorage.setItem('TestCasesData', JSON.stringify([...array, object]));
+    } else {
 
+        localStorage.setItem('TestCasesData', JSON.stringify([object]));
+    };
 
 };
